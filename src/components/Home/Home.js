bf21f2a8e4778064c16ca757f017b530cb1e317e
Fernando Ama <fernandoama95@outlook.es>
2019-11-30 14:16:16 -0600

    Iniciando con el 2do flujo

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -5,14 +5,7 @@ class Home extends Component {
     return (
       <div>
         <h2>Hola, bienvenido al administrador de venta del producto Lifemiles MarketPlace.</h2>
-        <p>Cras facilisis urna ornare ex volutpat, et
-        convallis erat elementum. Ut aliquam, ipsum vitae
-        gravida suscipit, metus dui bibendum est, eget rhoncus nibh
-        metus nec massa. Maecenas hendrerit laoreet augue
-        nec molestie. Cum sociis natoque penatibus et magnis
-        dis parturient montes, nascetur ridiculus mus.</p>
- 
-        <p>Duis a turpis sed lacus dapibus elementum sed eu lectus.</p>
+        <h3>¡Vive tus millas ¡Gana millas en todo lo que haces, todos los días!</h3>
       </div>
     );
   }
--- src/components/ListProducts/ListProducts.js
+++ src/components/ListProducts/ListProducts.js
@@ -10,172 +10,42 @@ export default class ListProducts extends Component {
   constructor(props) {
     super(props);
     this.sayHello = this.sayHello.bind(this);
-    this.handleChange = this.handleChange.bind(this);
-    this.handleChangeEmpresa = this.handleChangeEmpresa.bind(this);
     this.state = {
-      value: '',
-      valuePass: '',
-      valueEmpresa:'',
-      valueAmount:'',
-      valueImagen:'',
-      valueMoneda:'',
-      showResults : false
+      value: ''
     };
   }
 
-  handleChange(event) {
-    this.setState({value: event.target.value});
-  }
-
-  handleChangeEmpresa(event) {
-    this.setState({valuePass: event.target.value});
-  }
-
-  checkFormValidity(passwordValue, userNameValue) {
-		if (userNameValue && passwordValue) {
-      if(this.userNameValidation(userNameValue)){    
-      return true;
-      }
-		} else {
-      return false;
-		}
-	}
-	userNameValidation(value) {
-		let regex_emailNumberValue = new RegExp(/^[a-z]+$/g);
-		if (value.length >= 5) {
-      if (regex_emailNumberValue.test(value)) {
-        return true;
-      }else{
-        return false;
-      }
-		}else{
-      return false;
-    }
-
-	}
-
   sayHello() {
-    const {
-      value,
-      valuePass
-		} = this.state;
-    let validate = false;
-    this.setState({
-			userNameValue: value,
-			passwordValue: valuePass,
-		});	
-    validate = this.checkFormValidity(valuePass, value);
     this.makeLogIn(validate);
   }
   
-  makeLogIn(validate){
+  makeRequest(){
     const {
       value,
       valuePass
 		} = this.state;
   if(validate){
-    const url ='qwertyuio';
+    const url ='https://private-ce4b1-lmdev.apiary-mock.com/country/language/cms/v1/lac/log-in/products.entity.json';
     axios({
-      method: 'POST',
-      url,
-      headers: {
-        Accept: 'application/json',
-        ['Content-Type']: 'application/json'
-      }, 
-      data: {
-        user: value, // This is the body part
-        password: valuePass,
-    }
+      method: 'get',
+      url
       }).then((response) => {
         if(response.status == 200){
-          window.location.reload(false);
+          console.log('200 http status resp');
         }else{
-          console.log("Invalid Credentials");
-          this.setState({ showResults: true });
+          if(response.status == 200){
+            console.log('202 http status resp');
+          }else{
+              window.location.reload(false);
+          }
         }
     });
   }
 }
 
-isError(val) {
-  if(val){
-  return (
-      <div id="results" className="search-results">
-        <h2>Invalid Credentials</h2>
-      </div>
-  );
-  }else{
-    return null;
-  }
-}
-
 render() {
   return (
-    <div className="contentPage">
-      <div className="contentForm">        
-          <div><h2 id = "MainTitle">MarketPlace</h2></div>
-          <div><h3 id = "SubTitle">Add new product</h3></div>
-          <FormGroup>
-            <Label for="Empresa">Empresa:</Label>
-            <Input 
-              type="text"
-              value={this.state.value} 
-              onChange={this.handleChange} 
-              ref={(c) => this.empresa = c} 
-              name="empresa"
-              placeholder="Empresa"
-              /> 
-          </FormGroup>
-          <FormGroup>
-            <Label for="product_title">Titulo del producto</Label>
-            <Input 
-              type="product_title"
-              placeholder="Titulo"
-              value={this.state.valuePass} 
-              onChange={this.handleChangePass} 
-              ref={(c) => this.password = c} 
-              name="password"
-              />
-          </FormGroup>
-          <FormGroup>
-            <Label for="password">Password</Label>
-            <Input 
-              type="password"
-              placeholder="********"
-              value={this.state.valuePass} 
-              onChange={this.handleChangePass} 
-              ref={(c) => this.password = c} 
-              name="password"
-              />
-          </FormGroup>
-          <FormGroup>
-            <Label for="password">Password</Label>
-            <Input 
-              type="password"
-              placeholder="********"
-              value={this.state.valuePass} 
-              onChange={this.handleChangePass} 
-              ref={(c) => this.password = c} 
-              name="password"
-              />
-          </FormGroup>
-          <FormGroup>
-            <Label for="password">Password</Label>
-            <Input 
-              type="password"
-              placeholder="********"
-              value={this.state.valuePass} 
-              onChange={this.handleChangePass} 
-              ref={(c) => this.password = c} 
-              name="password"
-              />
-          </FormGroup>
-          <div>
-            <Button color="danger" id="SignIn"  onClick = {this.sayHello}>Add new product</Button>
-          </div>
-          { this.isError(this.state.showResults)}
-        </div>
-      </div>
+    <table></table>
     ) 
   }
 }
